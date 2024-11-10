825a1a8685ec52108585dbd5dbd11b54564a3f14
Vinícius Salles <vsmiranda4321@gmail.com>
2024-11-10 14:33:34 -0300

    feat: adicionando estilos e responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { useEffect } from "react";
 import "./App.css";
 import bolites2 from "./assets/img2.jpeg";
 import logobolites from "./assets/logobolites.png";
 import { useState } from "react";
-
+import img1 from "./assets/img1.jpeg";
+import img2 from "./assets/img2.jpeg";
 import video from "./assets/videoBolites.mp4";
+import { CiInstagram } from "react-icons/ci";
+import logo from "./assets/logo.png";
+import rotulagem from "./assets/rotulagem.png";
+import rotulagem2 from "./assets/rotulagem2.png";
+
 const App = () => {
-  const [porcentagem, setPorcentagem] = useState(100);
-  useEffect(() => {
-    const intervalo = setInterval(() => {
-      setPorcentagem((prev) => (prev > 0 ? prev - 1 : 0));
-    }, 10);
-    if (porcentagem === 0) clearInterval(intervalo);
-    return () => clearInterval(intervalo);
-  }, [porcentagem]);
+  const [dark, setDark] = useState("Dark");
+
+  const handleDark = () => {
+    setDark(dark === "Dark" ? "Light" : "Dark");
+    document.body.style.backgroundColor = dark === "Dark" ? "#333" : "#fff";
+  };
+
   return (
     <>
       <nav>
         <h1 className="h1Logo">
-          <img src={logobolites} alt="" height={50} width={50} />
+          <img src={logobolites} alt="" className="logobolites" />
           <p
             style={{
               fontSize: "10px",
@@ -34,7 +38,14 @@ const App = () => {
 
         <ul>
           <li>
-            <a href="">Comprar</a>
+            <a
+              href=""
+              onClick={() => {
+                alert("Em breve");
+              }}
+            >
+              Comprar
+            </a>
           </li>
           <li>
             <a href="">Sobre</a>
@@ -42,56 +53,56 @@ const App = () => {
           <li>
             <a href="">Contato</a>
           </li>
-
           <li>
-            <button className="butonDark">Tema Dark</button>
+            <button className="butonDark" onClick={handleDark}>
+              {dark}
+            </button>
           </li>
         </ul>
       </nav>
 
       <section className="section-desc-logo">
         <div>
-          <img
-            src={bolites2}
-            alt=""
-            width={400}
-            height={550}
-            className="img-logo"
-          />
+          <img src={bolites2} alt="" className="img-logo" />
         </div>
 
         <div className="desk">
           <h2>Bolites, o novo e inovador produto</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac
-            faucibus ligula. Sed facilisis, velit vel viverra consectetur, nunc
-            nulla fringilla nunc, sed tristique velit nunc vel justo. Donec
-            euismod auctor mauris, non tempus ex lacinia in.
+            É um lanche saudável e sustentável com ingredientes simples e
+            nutritivos, garantindo sabor e valor nutricional. Livre de glúten e
+            lactose, o Bolites atende pessoas com alergias ou intolerâncias
+            alimentares. O preparo foi planejado para ser rápido e acessível,
+            oferecendo uma opção prática para o dia a dia
           </p>
           <div className="button-sec">
-            <button>Saiba mais</button>
-            <button>Comprar</button>
+            <button
+              onClick={() => {
+                alert("Em breve");
+              }}
+            >
+              Comprar
+            </button>
           </div>
         </div>
       </section>
 
       <section className="nutricional">
         <div className="nutriDiv">
-          <span>{porcentagem}%</span>
+          <span>0%</span>
           <p>Lactose</p>
         </div>
         <div className="nutriDiv">
-          <span>{porcentagem}%</span>
+          <span>0%</span>
           <p>Glúten</p>
         </div>
         <div className="nutriDiv">
-          <span>{porcentagem}%</span>
+          <span>0%</span>
           <p>Conservantes</p>
         </div>
       </section>
 
       <section className="ingredientes">
-        <h3>Ingredientes</h3>
         <div className="ingredientes-div">
           <div>
             <video height={600} controls>
@@ -99,18 +110,98 @@ const App = () => {
             </video>
           </div>
           <div>
-            <h4>Ingre</h4>
+            <h4>Ingredientes</h4>
             <ul>
-              <li>pao</li>
-              <li>farinha</li>
-              <li>fermento</li>
-              <li>batata</li>
-              <li>agua</li>
-              <li>oleo</li>
+              <li>Batata Doce</li>
+              <li>Peito de Frango</li>
+              <li>Farinha de Arroz</li>
+              <li>Ovo</li>
+              <li>Sal</li>
+              <li>Azeite</li>
+              <li>Alho</li>
+              <li>Orange Pepper</li>
+              <li>Cúrcuma</li>
+              <li>Cebola</li>
+              <li>Cebolinha</li>
+              <li>Salsa</li>
+              <li>Tomilho</li>
             </ul>
+            <div className="rotulagem">
+              <img src={rotulagem} alt="" />
+              <img src={rotulagem2} alt="" />
+            </div>
           </div>
         </div>
       </section>
+
+      <section className="sections-photos">
+        <h4>Mais Algumas Fotos do Produto</h4>
+        <div className="photos">
+          <img src={img1} alt="" />
+          <img src={logo} alt="" />
+          <img src={img2} alt="" />
+        </div>
+      </section>
+
+      <section className="contato">
+        <h3>Entre em Contato</h3>
+        <form className="contato-form">
+          <input type="text" placeholder="Nome" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Mensagem" />
+          <button>Enviar</button>
+        </form>
+      </section>
+
+      <section className="sections-nutris">
+        <div className="nutris">
+          <h2>Juliana</h2>
+          <a
+            href="https://www.instagram.com/julianaContii/"
+            target="_blank"
+            className="nutri-insta"
+          >
+            <CiInstagram />
+          </a>
+        </div>
+        <div className="nutris">
+          <h2>Kethlen</h2>
+          <a
+            href="https://www.instagram.com/kethlen__lima/"
+            target="_blank"
+            className="nutri-insta"
+          >
+            <CiInstagram />
+          </a>
+        </div>
+        <div className="nutris">
+          <h2>Débora</h2>
+          <a
+            href="https://www.instagram.com/debora_faraco/"
+            target="_blank"
+            className="nutri-insta"
+          >
+            <CiInstagram />
+          </a>
+        </div>
+        <div className="nutris">
+          <h2>Bruna</h2>
+          <a
+            href="https://www.instagram.com/brunacarius/"
+            target="_blank"
+            className="nutri-insta"
+          >
+            <CiInstagram />
+          </a>
+        </div>
+      </section>
+
+      <footer>
+        <p>
+          © Vinícius Salles <br />
+          Alguns Direitos Reservados.
+        </p>
+      </footer>
     </>
   );
 };
